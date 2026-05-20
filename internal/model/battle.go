902f4a17e337e641357a57dfa7956d7dfa0043ed
Caio <caio_leone@hotmail.com>
2026-05-19 21:46:30 -0300

    feat: battleModel

---
+++ internal/model/battle.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type BattleModel struct {
+	id          uuid.UUID
+	attackerId  uuid.UUID
+	defenderId  uuid.UUID
+	winnerId    uuid.UUID
+	damageDealt int
+	createdAt   time.Time
+}

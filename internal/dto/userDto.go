0ce4f2dcb9523e4b34cbbd952dab3d35a51901cc
Caio <caio_leone@hotmail.com>
2026-05-20 19:59:27 -0300

    feat: userDto

---
+++ internal/dto/userDto.go
@@ -0,0 +1,31 @@
+package main
+
+import "github.com/google/uuid"
+
+type UserCreateRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6"`
+}
+
+type UserLoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type UserUpdateRequest struct {
+	Name string `json:"name" validate:"required,min=1,max=25"`
+}
+
+// RESPONSE
+
+type UserResponse struct {
+	Id    uuid.UUID `json:"id"`
+	Name  string    `json:"name"`
+	Email string    `json:"email"`
+}
+
+type LoginResponse struct {
+	AccessToken  string       `json:"access_token"`
+	RefreshToken string       `json:"refresh_token"`
+	User         UserResponse `json:"user"`
+}

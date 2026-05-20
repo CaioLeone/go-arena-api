abf07073ef1896defaff9047baec96a1c2e1cf8e
Caio <caio_leone@hotmail.com>
2026-05-19 21:43:13 -0300

    feat: characterModel

---
+++ internal/model/character.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/google/uuid"
+
+type CharacterModel struct {
+	id            uuid.UUID
+	userId        uuid.UUID
+	name          string
+	class         string
+	level         int
+	hp            int
+	attack        int
+	defense       int
+	rankingPoints int
+}

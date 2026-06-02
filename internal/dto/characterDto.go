466002f2348d4105f9c2fee2d8b9fa50d310ca65
Caio <caio_leone@hotmail.com>
2026-06-02 11:26:09 -0300

    fix: characterDto

--- internal/dto/characterDto.go
+++ internal/dto/characterDto.go
@@ -23,17 +23,18 @@ type CharacterUpdateRequest struct {
 // RESPONSE
 
 type CharacterResponse struct {
-	ID            uuid.UUID `json:"id"`
-	UserID        uuid.UUID `json:"user_id"`
-	Name          string    `json:"name"`
-	Class         string    `json:"class"`
-	Level         int       `json:"level"`
-	HP            int       `json:"hp"`
-	Attack        int       `json:"attack"`
-	Defense       int       `json:"defense"`
-	RankingPoints int       `json:"ranking_points"`
-	CreatedAt     string    `json:"created_at"`
-	UpdatedAt     string    `json:"updated_at"`
+	ID             uuid.UUID `json:"id"`
+	UserID         uuid.UUID `json:"user_id"`
+	Name           string    `json:"name"`
+	Class          string    `json:"class"`
+	Level          int       `json:"level"`
+	HP             int       `json:"hp"`
+	Attack         int       `json:"attack"`
+	Defense        int       `json:"defense"`
+	RankingPoints  int       `json:"ranking_points"`
+	CriticalChance int       `json:"critical_chance"`
+	CreatedAt      string    `json:"created_at"`
+	UpdatedAt      string    `json:"updated_at"`
 }
 
 type CharacterListResponse struct {

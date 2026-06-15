6eea00e75222c39272b07672d9ac67b78e5fc336
Caio <caio_leone@hotmail.com>
2026-06-15 10:02:26 -0300

    feat: rankingDto

---
+++ internal/dto/rankingDto.go
@@ -0,0 +1,21 @@
+package dto
+
+type UserRankingResponse struct {
+    CharacterID uuid.UUID `json:"character_id"`
+    Name        string    `json:"name"`
+    Class       string    `json:"class"`
+    Level       int       `json:"level"`
+    Rank        int64     `json:"rank"`
+    Score       int64     `json:"score"`
+}
+
+type TopPlayersResponse struct {
+    Rank  int64  `json:"rank"`
+    Name  string `json:"name"`
+    Score int64  `json:"score"`
+}
+
+type LeaderboardResponse struct {
+    Players []TopPlayersResponse `json:"players"`
+    Total   int                  `json:"total"`
+}

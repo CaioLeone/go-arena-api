a140ea96183ba04a732dd693e45197041b625a44
Caio <caio_leone@hotmail.com>
2026-06-15 10:06:57 -0300

    fix: ranking dto

--- internal/dto/rankingDto.go
+++ internal/dto/rankingDto.go
@@ -1,21 +1,23 @@
 package dto
 
+import "github.com/google/uuid"
+
 type UserRankingResponse struct {
-    CharacterID uuid.UUID `json:"character_id"`
-    Name        string    `json:"name"`
-    Class       string    `json:"class"`
-    Level       int       `json:"level"`
-    Rank        int64     `json:"rank"`
-    Score       int64     `json:"score"`
+	CharacterID uuid.UUID `json:"character_id"`
+	Name        string    `json:"name"`
+	Class       string    `json:"class"`
+	Level       int       `json:"level"`
+	Rank        int64     `json:"rank"`
+	Score       int64     `json:"score"`
 }
 
 type TopPlayersResponse struct {
-    Rank  int64  `json:"rank"`
-    Name  string `json:"name"`
-    Score int64  `json:"score"`
+	Rank  int64  `json:"rank"`
+	Name  string `json:"name"`
+	Score int64  `json:"score"`
 }
 
 type LeaderboardResponse struct {
-    Players []TopPlayersResponse `json:"players"`
-    Total   int                  `json:"total"`
+	Players []TopPlayersResponse `json:"players"`
+	Total   int                  `json:"total"`
 }

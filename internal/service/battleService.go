0d6be11cbb87bf7ec15a94943d26bd0073dc9db2
Caio <caio_leone@hotmail.com>
2026-06-16 09:46:08 -0300

    fix: battleservice startbattle func

--- internal/service/battleService.go
+++ internal/service/battleService.go
@@ -81,7 +81,7 @@ func (s *battleService) StartBattle(userID string, req *dto.BattleCreateRequest)
 	if err != nil {
 		return nil, fmt.Errorf("Erro ao Salvar Batalha: %w", err)
 	}
-	
+
 	//Atualizar Ranking se nao foi empate
 	if !battleResult.IsDraw && battleResult.Winner != nil {
 		winnerRanking, _ := s.battleRepo.GetCharacterRanking(battleResult.Winner.ID.String())
@@ -108,18 +108,6 @@ func (s *battleService) StartBattle(userID string, req *dto.BattleCreateRequest)
 			battleResult.Loser.Name,
 			newLoserScore,
 		)
-	}
-
-
-	if !battleResult.IsDraw && battleResult.Winner != nil {
-		winnerRanking, _ := s.battleRepo.GetCharacterRanking(string(battleResult.Winner.ID.String()))
-		loserRanking, _ := s.battleRepo.GetCharacterRanking(string(battleResult.Loser.ID.String()))
-
-		RankingDiff := winnerRanking - loserRanking
-		pointsGained := battle.UpdateRanking(battleResult.Winner, battleResult.Loser, RankingDiff)
-
-		s.battleRepo.UpdateCharacterRanking(battleResult.Winner.ID.String(), pointsGained)
-		s.battleRepo.UpdateCharacterRanking(battleResult.Loser.ID.String(), -5) //Perde 5 Pontos
 	}
 
 	rounds, _ := battle.FromRoundsJSON(roundsJSON)

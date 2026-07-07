d1dfc8a2ead55cf5ffbbf2fdb0625ea466de21f7
Caio <caio_leone@hotmail.com>
2026-07-07 09:49:30 -0300

    fix: docs.go swagger

---
+++ cmd/api/docs.go
@@ -0,0 +1,18 @@
+package main
+
+// @title Go Arena API
+// @version 1.0
+// @description API REST para gerenciamento de batalhas entre personagens.
+// @description Projeto desenvolvido em Go utilizando Gin, PostgreSQL, Redis e JWT.
+
+// @contact.name Caio Soeiro
+// @contact.url https://github.com/caioLeone
+
+// @license.name MIT
+
+// @host localhost:8080
+// @BasePath /
+
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization

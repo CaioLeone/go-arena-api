460ad70f4e1ced0d8bd4917e7ab8858a5c3adf27
Caio <caio_leone@hotmail.com>
2026-07-14 09:59:49 -0300

    fix: changed characterModel

--- internal/model/character.go
+++ internal/model/character.go
@@ -7,16 +7,18 @@ import (
 )
 
 type CharacterModel struct {
-	ID             uuid.UUID
-	UserID         uuid.UUID
-	Name           string
-	Class          string
-	Level          int
-	HP             int
-	Attack         int
-	Defense        int
-	RankingPoints  int
-	CriticalChance int
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
+	ID              uuid.UUID
+	UserID          uuid.UUID
+	Name            string
+	Class           string
+	Level           int
+	Experience      int
+	HP              int
+	Attack          int
+	Defense         int
+	AttributePoints int
+	RankingPoints   int
+	CriticalChance  int
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
 }

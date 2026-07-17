5f46491d4914639e3bb13864af9a0be4b7716712
Caio <caio_leone@hotmail.com>
2026-07-17 10:15:55 -0300

    fix: progression consts

--- internal/service/progression.go
+++ internal/service/progression.go
@@ -1,6 +1,16 @@
 package service
 
-	const (
-		WinnerExperience = 20
-		LoserExperience  = 10
-	)
+const (
+	//Experiencia
+	ExperienceToLevelUp = 100 // Define a quantidade de experiência necessária para subir de nível
+
+	//Recompensas de Experiencia de Batalha
+	WinnerExperience = 20
+	LoserExperience  = 10
+
+	//Progressao
+	AttributesPointsPerLevel = 1
+
+	//Limites
+	MaxCharacterLevel = 100
+)

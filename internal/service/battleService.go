72cc24a9fad25b1e07a176966a6a199907241b3f
Caio <caio_leone@hotmail.com>
2026-07-17 10:12:28 -0300

    fix: battleService consts

--- internal/service/battleService.go
+++ internal/service/battleService.go
@@ -180,11 +180,6 @@ func (s *battleService) StartBattle(userID string, req *dto.BattleCreateRequest)
 
 	s.updateRanking(battleResult)
 
-	const (
-		WinnerExperience = 20
-		LoserExperience  = 10
-	)
-
 	if !battleResult.IsDraw {
 		s.characterService.AddExperience(battleResult.Winner.ID.String(), WinnerExperience)
 		s.characterService.AddExperience(battleResult.Loser.ID.String(), LoserExperience)

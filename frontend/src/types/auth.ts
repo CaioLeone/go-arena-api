3a44e3e3720722ac87c1b19913ee19d164179338
Caio <caio_leone@hotmail.com>
2026-07-21 10:08:15 -0300

    fix: auth.ts added interfaces

--- frontend/src/types/auth.ts
+++ frontend/src/types/auth.ts
@@ -1,3 +1,9 @@
+export interface RegisterRequest{
+    username: string;
+    email: string;
+    password: string;
+}
+
 export interface LoginRequest {
     email: string;
     password: string;
@@ -17,4 +23,12 @@ export interface User{
     id: string;
     username: string;
     email: string;
+}
+
+export interface AuthContextData{
+    user: User | null;
+    token: string | null;
+    isAuthenticated: boolean;
+    login: (email: string, password: string) => Promise<void>;
+    logout: () => void;
 }

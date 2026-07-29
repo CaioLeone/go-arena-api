d246b5cfe09b563872abc178ce99f76b1f7f0eb7
Caio <caio_leone@hotmail.com>
2026-07-29 18:59:17 -0300

    feat: Leaderboard tsx

---
+++ frontend/src/pages/Leaderboard.tsx
@@ -0,0 +1,9 @@
+import DashboardLayout from "../components/layout/DashboardLayout";
+
+export default function Leaderboard(){
+    return(
+        <DashboardLayout>
+            <h1>Ranking</h1>
+        </DashboardLayout>
+    );
+}

979b52d2559b965aeec7f7ca1dd12442eee87189
Caio <caio_leone@hotmail.com>
2026-07-29 18:56:17 -0300

    fix: dashboard export

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,12 +1,15 @@
+import DashboardLayout from "../components/layout/DashboardLayout";
 import { useAuth } from "../hooks/useAuth";
 
 export default function Dashboard() {
     const {user, logout} = useAuth();
     return (
-        <div>
-            <h1>Dashboard</h1>
-            <p>Bem Vindo, {user?.name}</p>
-            <button onClick={logout}>Sair</button>
-        </div>
+        <DashboardLayout>
+            <div>
+                <h1>Dashboard</h1>
+                <p>Bem Vindo, {user?.name}</p>
+                <button onClick={logout}>Sair</button>
+            </div>
+        </DashboardLayout>
     );
 }

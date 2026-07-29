43a5a9b06746a6988ee3a4d52b0f05bf873f82f7
Caio <caio_leone@hotmail.com>
2026-07-29 18:57:41 -0300

    feat: character.tsx

---
+++ frontend/src/pages/Characters.tsx
@@ -0,0 +1,9 @@
+import DashboardLayout from "../components/layout/DashboardLayout";
+
+export default function Characters(){
+    return(
+        <DashboardLayout>
+            <h1>Guerreiros</h1>
+        </DashboardLayout>
+    );
+}

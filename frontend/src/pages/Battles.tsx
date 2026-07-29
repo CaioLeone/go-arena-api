40f7f3ed14d70a99e0778f16e5477f990115aa6c
Caio <caio_leone@hotmail.com>
2026-07-29 18:58:16 -0300

    feat: batalhas tsx

---
+++ frontend/src/pages/Battles.tsx
@@ -0,0 +1,9 @@
+import DashboardLayout from "../components/layout/DashboardLayout";
+
+export default function Battles(){
+    return(
+        <DashboardLayout>
+            <h1>Batalhas</h1>
+        </DashboardLayout>
+    );
+}

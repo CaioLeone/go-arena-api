ec0b47a253789ae3752c33d85d71fcbb8f69b0ee
Caio <caio_leone@hotmail.com>
2026-07-29 19:08:33 -0300

    fix: index added characters, battles and leaderboard routes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,6 +3,9 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Dashboard from "../pages/Dashboard";
+import Characters from "../pages/Characters";
+import Battles from "../pages/Battles";
+import Leaderboard from "../pages/Leaderboard";
 
 import { PrivateRoute } from "../components/PrivateRoute";
 
@@ -10,23 +13,38 @@ export default function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route 
-                    path="/" 
-                    element={<Home />}
-                />
-                <Route 
-                    path="/login" 
-                    element={<Login />}
-                />
-                <Route 
-                    path="/register" 
-                    element={<Register />}
-                />
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
                 <Route 
                     path="/dashboard" 
                     element={
                         <PrivateRoute> 
                             <Dashboard/>
+                        </PrivateRoute>}
+                />
+
+                <Route 
+                    path="/characters" 
+                    element={
+                        <PrivateRoute> 
+                            <Characters/>
+                        </PrivateRoute>}
+                />
+
+                <Route 
+                    path="/battles" 
+                    element={
+                        <PrivateRoute> 
+                            <Battles/>
+                        </PrivateRoute>}
+                />
+
+                <Route 
+                    path="/leaderboard" 
+                    element={
+                        <PrivateRoute> 
+                            <Leaderboard/>
                         </PrivateRoute>}
                 />
             </Routes>

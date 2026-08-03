35a11ec5856a46da6d13654f5866f3e15353a3a9
Caio <caio_leone@hotmail.com>
2026-08-03 10:59:02 -0300

    fix: character page

--- frontend/src/pages/Characters.tsx
+++ frontend/src/pages/Characters.tsx
@@ -1,9 +1,11 @@
 import DashboardLayout from "../components/layout/DashboardLayout";
+import CharactersList from "../components/characters/CharacterList";
 
 export default function Characters(){
     return(
         <DashboardLayout>
             <h1>Guerreiros</h1>
+            <CharactersList/>
         </DashboardLayout>
     );
 }

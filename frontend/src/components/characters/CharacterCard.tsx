2dbc838aa54355b5bd1abd5c77e1d4dc71e1640f
Caio <caio_leone@hotmail.com>
2026-08-05 11:28:44 -0300

    fix: characterCard

--- frontend/src/components/characters/CharacterCard.tsx
+++ frontend/src/components/characters/CharacterCard.tsx
@@ -1,4 +1,5 @@
 import type { Character } from "../../types/character";
+import {Link} from "react-router-dom";
 
 interface Props{
     character: Character;
@@ -6,23 +7,28 @@ interface Props{
 
 export default function CharacterCard({ character }: Props){
     return (
-        <div
-            style={{
-                border: "1px solid #ccc",
-                borderRadius: 8,
-                padding: 20,
-                marginBottom: 15,
-                background: "#fff",
-            }}
-        >
-            <h2>{character.name}</h2>
-            <p>Classe: {character.class}</p>
-            <p>Nivel: {character.level}</p>
-            <p>HP: {character.hp}</p>
-            <p>Ataque: {character.attack}</p>
-            <p>Defesa: {character.defense}</p>
-            <p>Experiencia: {character.experience}</p>
-            <p>Pontos de Atributo: {character.attribute_points}</p>
-        </div>
+        <link to={`/characters/${character.id}`}>
+            <CharacterCard character={character} />
+            <div
+                style={{
+                    border: "1px solid #ccc",
+                    borderRadius: 8,
+                    padding: 20,
+                    marginBottom: 15,
+                    background: "#fff",
+                }}
+            >
+                <h2>{character.name}</h2>
+                <p>Classe: {character.class}</p>
+                <p>Nivel: {character.level}</p>
+                <p>HP: {character.hp}</p>
+                <p>Ataque: {character.attack}</p>
+                <p>Defesa: {character.defense}</p>
+                <p>Chance de Critico: {character.critical_chance}</p>
+                <p>Experiencia: {character.experience}</p>
+                <p>Pontos de Atributo: {character.attribute_points}</p>
+            </div>
+        </link>
+        
     );
 }

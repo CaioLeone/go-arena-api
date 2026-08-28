18518ab8d03a38b9cc3383926e48dfad2a820316
Caio Leone Fonseca Casanova Soeiro <caio_leone@hotmail.com>
2026-08-28 11:11:04 -0300

    feat: battle type

---
+++ frontend/src/types/battle.ts
@@ -0,0 +1,59 @@
+export interface BattleCreateRequest {
+    attacker_character_id: number;
+    defender_character_id: number;
+}
+
+export interface BattleRound{
+    round: number;
+    attacker_name: string;
+    defender_name: string;
+    damage: number;
+    remaining_hp: number;
+    is_critical: boolean;
+    message: string;
+}
+
+export interface Battle {
+    id: string;
+
+    attacker_id: string;
+    attacker_name: string;
+
+    defender_id: string;
+    defender_name: string;
+
+    winner_id: string | null;
+    winner_name: string;
+
+    damage_dealt: number;
+
+    attacker_hp_final: number;
+    defender_hp_final: number;
+
+    rounds_count: number;
+    rounds: BattleRound[];
+
+    created_at: string;
+}
+
+export interface BattleHistory {
+    id: string;
+
+    attacker_id: string;
+    attacker_name: string;
+
+    defender_id: string;
+    defender_name: string;
+
+    winner_id: string | null;
+    winner_name: string;
+
+    damage_dealt: number;
+
+    attacker_hp_final: number;
+    defender_hp_final: number;
+
+    rounds_count: number;
+
+    created_at: string;
+}

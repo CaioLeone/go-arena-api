37711903d7e15c3b2a70098c880e6a7e63ea7abf
Caio Leone Fonseca Casanova Soeiro <caio_leone@hotmail.com>
2026-09-14 11:34:54 -0300

    fix: api interceptors

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -13,9 +13,14 @@ api.interceptors.request.use(
     (config)=> {
         const token = localStorage.getItem("@arena:token");
 
+        console.log("TOKEN ENVIADO:", token);
+        
         if(token){
             config.headers.Authorization = `Bearer ${token}`;
         }
+
+        console.log("REQUEST:", config.url);
+        
         return config;
     },
     (error) => {
